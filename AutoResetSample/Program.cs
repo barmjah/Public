cbb093c144417ddb3090a59c814171f2a1c7a34b
barmjah <32972841+barmjah@users.noreply.github.com>
2017-10-26 06:26:34 +0800

    تعرف إلى تنظيم السياقات بواسطة AutoResetEvent
    تعرف إلى كيفية تنظيم السياقات Threads من خلال أحد مفاهيم العبور المطبق باستخدام الفئة AutoResetEvent

---
+++ AutoResetSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace AutoResetSample
+{
+    class Program
+    {
+        private static AutoResetEvent autoReset;
+
+        static Program()
+        {
+            // false indicates that the initial-state is not signaled
+            autoReset = new AutoResetEvent(true);
+        }
+        static void Main(string[] args)
+        {
+            WriteLine("the application has started!");
+
+            while(true){
+                new Thread(new ThreadStart(Calculate)).Start();
+                //Task.Run(() => Calculate());
+                autoReset.WaitOne();
+                WriteLine("’Method call was completed!");
+            }
+        }
+
+        private static void Calculate(){
+            WriteLine("Sleep is going to be called!");
+            Thread.Sleep(5000);
+            autoReset.Set();
+            WriteLine("Set called");
+        }
+    }
+}
