7e7eb38553f9bd44a38d6591abd664e225d362b0
barmjah <32972841+barmjah@users.noreply.github.com>
2017-10-26 06:27:30 +0800

    تعرف إلى تنظيم السياقات من خلال ManualResetEvent
    تعرف إلى كيفية تنظيم السياقات من خلال مفهوم العبور المطبق من خلال في الفئة ManualResetEvent

---
+++ ManualResetEventSample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace ManualResetEventSample
+{
+    class Program
+    {
+        static ManualResetEvent manualEvent;
+
+        static Program() => manualEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            int i = 3;
+            while (i-- > 0) {
+                var t = new Thread(new ThreadStart(ThreadProc));
+                t.Name = $"Thread_{i}";
+                t.Start();
+            }
+
+            Console.WriteLine("Press Enter to send a signal to allow awating thread");
+            Console.ReadKey();
+
+            manualEvent.Set();
+
+            Thread.Sleep(500);
+            
+            Console.WriteLine("Press Enter to put manualEvent in non-signal state");
+            Console.ReadKey();
+
+            manualEvent.Reset();
+
+            var t4 = new Thread(new ThreadStart(ThreadProc));
+            t4.Name = "Thread_4";
+            t4.Start();
+
+            Console.WriteLine("Press Enter to conclude the application and allow any left awaiting threads");
+            Console.ReadKey();
+
+            manualEvent.Set();
+        }
+
+        static void ThreadProc() {
+            string threadName = Thread.CurrentThread.Name;
+            Thread.Sleep(1000);
+
+            manualEvent.WaitOne();
+            Console.WriteLine($"thread: {threadName} has called fucntion: {nameof(ThreadProc)}");
+        }
+    }
+}
