7017b9671a387446942e7303eedc479ee75aec97
barmjah <32972841+barmjah@users.noreply.github.com>
2017-12-08 01:53:45 +0800

    Add files via upload

---
+++ EnterpriseLoggingSample/EnterpriseLoggingSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Logging;
+
+namespace EnterpriseLoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.SetLogWriter(new LogWriterFactory().Create());
+            Logger.Write("Hello World", "General");
+
+            //System.Diagnostics.Process.Start("trace.log");
+        }
+    }
+}
